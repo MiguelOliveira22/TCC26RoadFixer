5eaafecf10ea1f2a1a3e5dac05bfa280c5a52c2a
Matheus Franco <162757230+cc24609@users.noreply.github.com>
2026-06-11 10:23:49 -0300

    ajustes e exemplos de dados

--- RoadFixer/src/Pages/HomePage.jsx
+++ RoadFixer/src/Pages/HomePage.jsx
@@ -4,11 +4,11 @@ import Features from "../components/Features";
 import DataCollection from "../components/DataCollection";
 
 export default function HomePage() {
-  // Não temos url para os tipos de dados que ainda coletaremos
+  // Não temos url para os tipos de dados que ainda coletaremos (solução temporaria)
   const reports = [
-    { id: 1, data: ["Clima" , {url: ""}]},
-    { id: 2, data: ["Topologia" , {url: ""}]},
-    { id: 3, data: ["Estrutura física da pista" , {url: ""}]},
+    { id: 1, data: ["Clima e Condições Meteorológicas" , {url: "https://portal.inmet.gov.br/"}]},
+    { id: 2, data: ["Topologia e Georreferenciamento de Rodovias" , {url: "https://dados.gov.br/dados/conjuntos-dados/sistema-nacional-de-viacao-snv"}]},
+    { id: 3, data: ["Estrutura Física e Condição de Pistas" , {url: "https://www.gov.br/dnit/pt-br/assuntos/planejamento-e-pesquisa/pesquisa-de-rodovias"}]},
   ];
 
   return (
--- RoadFixer/src/components/DataSetTable.jsx
+++ RoadFixer/src/components/DataSetTable.jsx
@@ -3,14 +3,46 @@ import styles from "./DataSetTable.module.css";
 import ScaffoldButton from "./Button/ScaffoldButton";
 
 const datasets = [
-  {id: 1, data: "Clima", url: ""},
-  {id: 2, data: "Topologia", url: ""},
-  {id: 3, data: "Estrutura Física Da Pista", url: ""},
-  {id: 4, data: "Conjunto", url: ""},
-  {id: 5, data: "Conjunto", url: ""},
-  {id: 6, data: "Conjunto", url: ""},
-  {id: 7, data: "Conjunto", url: ""},
-  {id: 8, data: "Conjunto", url: ""}
+  { 
+    id: 1, 
+    data: "Clima e Condições Meteorológicas", 
+    url: "https://portal.inmet.gov.br/" 
+  },
+  { 
+    id: 2, 
+    data: "Topologia e Georreferenciamento de Rodovias", 
+    url: "https://dados.gov.br/dados/conjuntos-dados/sistema-nacional-de-viacao-snv" 
+  },
+  { 
+    id: 3, 
+    data: "Estrutura Física e Condição de Pistas", 
+    url: "https://www.gov.br/dnit/pt-br/assuntos/planejamento-e-pesquisa/pesquisa-de-rodovias" 
+  },
+  { 
+    id: 4, 
+    data: "Microdados de Acidentes em Rodovias Federais", 
+    url: "https://www.gov.br/prf/pt-br/acesso-a-informacao/dados-abertos/dados-abertos-da-prf" 
+  },
+  { 
+    id: 5, 
+    data: "Painel de Acidentes Rodoviários", 
+    url: "https://cnt.org.br/painel-acidente" 
+  },
+  { 
+    id: 6, 
+    data: "Dados Abertos de Infrações e Tráfego", 
+    url: "https://dadosabertos.artesp.sp.gov.br/" 
+  },
+  { 
+    id: 7, 
+    data: "Estatísticas de Acidentes e Volume de Tráfego", 
+    url: "https://www.der.sp.gov.br/WebSite/Servicos/DadosAbertos.aspx" 
+  },
+  { 
+    id: 8, 
+    data: "Dados de Frota de Veículos por Município", 
+    url: "https://www.gov.br/senatran/pt-br/assuntos/estatisticas/dados-estatisticas-frota-de-veiculos" 
+  }
 ];
 
 export function DatasetTable({ isOpen, onClose }) {
@@ -32,22 +64,25 @@ export function DatasetTable({ isOpen, onClose }) {
 
         <table className={styles.datasetTable}>
           <thead className={styles.datasetHeader}>
+            <tr> {/* Correção importante aqui */}
               <th className={styles.th}>CONJUNTO</th>
               <th className={styles.th}>LINK</th>
-          </thead>
-
-          {datasets.map((item) => (
-            <tr className={styles.datasetRow}>
-              <td>{item.data}</td>
-              <td>
-                <ScaffoldButton
-                  value = {"Detalhes"}
-                  action = {() => window.open(item.url, '_blank')}
-                  orange = {false}
-                />
-              </td>
             </tr>
-          ))}
+          </thead>
+          <tbody> {/* Correção estrutural importante aqui */}
+            {datasets.map((item) => (
+              <tr key={item.id} className={styles.datasetRow}> {/* Adicionado o atributo 'key' */}
+                <td>{item.data}</td>
+                <td>
+                  <ScaffoldButton
+                    value={"Detalhes"}
+                    action={() => window.open(item.url, '_blank')}
+                    orange={false}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
