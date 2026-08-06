17d471164a4f09d78d49f1d67c0827912eba9d5b
Henrique Inoue <henriqueinoue1000@gmail.com>
2026-08-06 12:00:53 -0300

    calculo de risco
    n tem a formula, mas a estrutura está compatível.
    
    testeSalvo para n perder a base do teste

--- RoadFixerAPI/mapping/routes.py
+++ RoadFixerAPI/mapping/routes.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, PlainTextResponse
+from pathlib import Path
+from datetime import datetime
 
 import json
 import random
@@ -38,4 +40,52 @@ def assignRoutesAPI(api: FastAPI):
     async def getAccidentHistory():
         with open(filepath + "accident-history/content.json") as file:
             return json.load(file)
+
+    @api.get("/calcAccidents")
+    async def calcAccidents():
+        directory = Path(filepath + "accident-history/data")
         
+        with open(filepath + "accident-history/risk/savedData.json", "r+", encoding="utf-8") as savedFile:
+            savedData = json.load(savedFile)
+            houve_alteracao = False # Controle para só salvar se houver novos dados
+
+            for file_path in directory.iterdir():
+                print(file_path.name)
+                # Pulando arquivos ocultos ou diretórios acidentais (.DS_Store, etc)
+                if not file_path.is_file():
+                    continue
+
+                with open(file_path, "r", encoding="utf-8") as f:
+                    data = json.load(f)
+
+                    # Evita erro caso o arquivo lido esteja sem registros
+                    if not data.get("records"):
+                        continue
+
+                    # Ignora arquivos antigos baseado no last_update salvo
+                    if datetime.fromisoformat(data["records"][0][1]) < datetime.fromisoformat(savedData["last_update"]):
+                        continue
+
+                    houve_alteracao = True
+
+                    # Processa os registros de trás para frente
+                    for i in range(len(data["records"]) - 1, 0, -1):
+
+                        ''' Alterar a função que calcula o risco aqui'''
+
+                        indice_risk = round(float(data["records"][i][7].replace(",", ".")))
+                        
+                        if 0 <= indice_risk < len(savedData["risk"]):
+                            savedData["risk"][indice_risk] += 1
+
+                        savedData["last_update"] = data["records"][i][1]
+                        ''' Fim da função'''
+
+            # SALVAMENTO AUTOMÁTICO: Fora de todos os loops
+            if houve_alteracao:
+                print("att")
+                savedFile.seek(0)
+                savedFile.truncate()
+                json.dump(savedData, savedFile, indent=4) # json.dump (sem "s") grava no arquivo
+
+        return {"status": "Processamento concluído com sucesso"}
